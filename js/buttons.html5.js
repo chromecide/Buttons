dc1e5cadf90896c67d4a2f4978e1d1c745198fa4
Justin Pradier <chromecide@chromecide.com>
2018-05-22 12:07:04 +1000

    added new multi header support

--- js/buttons.html5.js
+++ js/buttons.html5.js
@@ -48,7 +48,61 @@ function _pdfMake () {
 	return pdfmake || window.pdfMake;
 }
 
+var _fnGetHeaders = function(dt) {
+    var thRows = $(dt.header()[0]).children();
+    var numRows = thRows.length;
+    var matrix = [];
 
+    // Iterate over each row of the header and add information to matrix.
+    for ( var rowIdx = 0;  rowIdx < numRows;  rowIdx++ ) {
+        var $row = $(thRows[rowIdx]);
+
+        // Iterate over actual columns specified in this row.
+        var $ths = $row.children("th");
+        for ( var colIdx = 0;  colIdx < $ths.length;  colIdx++ )
+        {
+            var $th = $($ths.get(colIdx));
+            var colspan = $th.attr("colspan") || 1;
+            var rowspan = $th.attr("rowspan") || 1;
+            var colCount = 0;
+
+            // ----- add this cell's title to the matrix
+            if (matrix[rowIdx] === undefined) {
+                matrix[rowIdx] = [];  // create array for this row
+            }
+            // find 1st empty cell
+            for ( var j = 0;  j < (matrix[rowIdx]).length;  j++, colCount++ ) {
+                if ( matrix[rowIdx][j] === "PLACEHOLDER" ) {
+                    break;
+                }
+            }
+            var myColCount = colCount;
+            matrix[rowIdx][colCount++] = $th.text();
+
+            // ----- If title cell has colspan, add empty titles for extra cell width.
+            for ( var j = 1;  j < colspan;  j++ ) {
+                matrix[rowIdx][colCount++] = "";
+            }
+
+            // ----- If title cell has rowspan, add empty titles for extra cell height.
+            for ( var i = 1;  i < rowspan;  i++ ) {
+                var thisRow = rowIdx+i;
+                if ( matrix[thisRow] === undefined ) {
+                    matrix[thisRow] = [];
+                }
+                // First add placeholder text for any previous columns.                 
+                for ( var j = (matrix[thisRow]).length;  j < myColCount;  j++ ) {
+                    matrix[thisRow][j] = "PLACEHOLDER";
+                }
+                for ( var j = 0;  j < colspan;  j++ ) {  // and empty for my columns
+                    matrix[thisRow][myColCount+j] = "";
+                }
+            }
+        }
+    }
+
+    return matrix;
+};
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  * FileSaver.js dependency
  */
